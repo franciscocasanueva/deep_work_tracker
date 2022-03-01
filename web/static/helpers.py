e818ae754c92ad693a7c08708759edbf93cfe673
Francisco Casanueva <pcv993@gmail.com>
2022-03-01 18:02:41 -0500

    First Working SQL alquemy + Postgress solution

--- web/static/helpers.py
+++ web/static/helpers.py
@@ -1,18 +1,22 @@
 from flask import session, redirect
 import functools
 
+
 def login_required(f):
     """
     Decorate routes to require login.
 
     https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
     """
+
     @functools.wraps(f)
     def decorated_fuction(*args, **kwargs):
         if session.get("user_id") is None:
             return redirect("/login")
         return f(*args, **kwargs)
+
     return decorated_fuction
+
 
 def get_unique(seq):
     """
@@ -22,6 +26,7 @@ def get_unique(seq):
     seen_add = seen.add
     return [x for x in seq if not (x in seen or seen_add(x))]
 
+
 def rolling_sum(list_input, window):
     # TODO: Change this function to a recursion.
     rolling_list = []
@@ -29,18 +34,18 @@ def rolling_sum(list_input, window):
         rolling_value = 0
         for w in range(window):
             if (i - w) > 0:
-                rolling_value += list_input[i-w]
-        rolling_list.append(rolling_value/float(window))
-    return(rolling_list)
+                rolling_value += list_input[i - w]
+        rolling_list.append(rolling_value / float(window))
+    return (rolling_list)
 
 
-def pull_dataset(db, days_to_pull, rolling_sum_window, users=None):
+def pull_dataset(conn, days_to_pull, rolling_sum_window, users=None):
     """
     From a list of users pull working minutes dataset
     """
     if not users:
-        rows = db.execute(
-        """
+        rows = conn.execute(
+            """
             SELECT distinct(id) as user_id
             FROM users;
         """)
@@ -51,7 +56,7 @@ def pull_dataset(db, days_to_pull, rolling_sum_window, users=None):
     users = "('" + "', '".join(users) + "')"
     days_to_pull_with_buffer = days_to_pull + rolling_sum_window
 
-    rows = db.execute(
+    rows = conn.execute(
         """
             SELECT username, calendar.d as date, coalesce(sum(number_sessions),0) as number_sessions
             FROM calendar
@@ -71,8 +76,8 @@ def pull_dataset(db, days_to_pull, rolling_sum_window, users=None):
 
     datasets = []
     for username in usernames:
-        dw_sessions = [x['number_sessions'] for x in rows if x['username']==username]
-        dw_minutes = [x*25 for x in dw_sessions]
+        dw_sessions = [x['number_sessions'] for x in rows if x['username'] == username]
+        dw_minutes = [x * 25 for x in dw_sessions]
         seven_day_average = rolling_sum(dw_minutes, rolling_sum_window)
         dataset = {}
         dataset['label'] = username
@@ -81,4 +86,3 @@ def pull_dataset(db, days_to_pull, rolling_sum_window, users=None):
         datasets.append(dataset)
 
     return labels, datasets
-
