f3fe58811d28aed72d0bb21125a07684079bce9f
Francisco Casanueva Villalon <franciscocasa@fb.com>
2022-10-02 11:29:40 +0100

    Add different time frames in social

--- web/db/config.py
+++ web/db/config.py
@@ -6,7 +6,4 @@ host = os.environ['POSTGRES_HOST']
 database = os.environ['POSTGRES_DB']
 port = os.environ['POSTGRES_PORT']
 
-DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@db/{database}'
-
-# This was the original URI and It was using the host and port fields. However It is failing. Using DB makes It work.
-# DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
+DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
